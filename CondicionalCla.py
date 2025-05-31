5db63afaa8da6371b557e87fc2b3e0b2fabc54b5
Shalo Moreno <salomemorenosanchez9@gmail.com>
2025-05-31 16:57:23 -0500

    Co

--- CondicionalCla.py
+++ CondicionalCla.py
@@ -3,5 +3,23 @@ if edad < 18:
     print("Usted es menor de edad.")
 else:
     print("Usted es mayor de edad.")
+
+print("----------Ejemplo 1----------")
+x=25
+if x > 10:
+    print("pon encima de diez,")
+    if x > 20:
+        print("y también por encima de 20!")
+    else:("pero no por encima de 20")
     
+
+print("----------Ejemplo 2----------")
+y = 15
+if y > 10:
+    print("por encima de diez")
+    if y > 20:
+        print("y también por encima de 20!")
+    else:
+        print("pero no por encima de 20")
+        
 
